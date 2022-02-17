a3fcf99336b359c6d5049fa969f5e58a8eef8b8d
Raphael Pour <info@raphaelpour.de>
2022-02-17 23:00:56 +0100

    TestLevel: Refactor

--- scenes/TestLevel.gd
+++ scenes/TestLevel.gd
@@ -6,11 +6,12 @@ var dice_sounds = [
 	preload("res://assets/audio/sound/dice_roll_3.mp3")
 ]
 
-const TURN_PLAYER_TEXT = "It's your turn!"
-const TURN_OPPONENT_TEXT = "It's Arthurs turn"
+export (globals.Team) var current_team = globals.Team.PLAYER
 
 func _ready():
-	$UI/Centered/Panel/TurnLabel.text = TURN_PLAYER_TEXT
+	$UI/Centered/Panel/TurnLabel.text = "Turn of %s" % globals.team_to_string(current_team)
+	for die in $Dice.get_children():
+		(die as Die).connect("rolled", self, "_on_dice_rolled")
 
 func _process(_delta):
 	if Input.is_action_just_pressed("r_key"):
@@ -18,21 +19,22 @@ func _process(_delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		$"UI/Centered/Panel/Stones-Opponent".emit_signal("stones_changed", 5)
 		$"UI/Centered/Panel/Stones-Player".emit_signal("stones_changed", 3)
-		
+
 func _roll_dice():
 	print_debug($GameController.currently_rolling)
 	
 	if $GameController.currently_rolling: return 
 		
 	play_dice_sound()
-	$GameController.roll_dice()
+    for die in $Dice.get_children():
+            (die as Die).roll()
+
+func _on_dice_rolled(number: int):
+	print_debug("rolled a " + str(number))
 
 func _on_GameController_switch_turn(turn_owner: int):
 	$TokenSound.play()
-	if turn_owner == globals.Team.PLAYER:
-		$UI/Centered/Panel/TurnLabel.text = TURN_PLAYER_TEXT
-	else:
-		$UI/Centered/Panel/TurnLabel.text = TURN_OPPONENT_TEXT
+	$TurnLabel.text = "Turn of %s" % globals.team_to_string(current_team)
 		
 	$GameController.currently_rolling = false
 	print_debug($GameController.currently_rolling)
--- scripts/globals.gd
+++ scripts/globals.gd
@@ -4,3 +4,9 @@ enum Team {
 	PLAYER
 	ENEMY
 }
+
+func team_to_string(team: int) -> String:
+	match team:
+		Team.PLAYER: return "Player"
+		Team.ENEMY: return "Enemy"
+		_: return "Unknown"
