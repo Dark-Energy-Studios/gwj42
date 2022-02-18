181085552a57050ed3e3ace5c6101fa73b5214db
Raphael Pour <info@raphaelpour.de>
2022-02-18 22:43:09 +0100

    BasicAI: make ai even more stupid
    The chips are pre-filtered so the ai just need to use the
    first one.

--- components/BaseAI/BaseAI.gd
+++ components/BaseAI/BaseAI.gd
@@ -7,12 +7,9 @@ var name = "Arth'Ur Dent"
 func _init():
 	pass
 	
-func make_move(dice_roll, ai_chips, player_chips):
-	var usable_chips = []
-	for chip in ai_chips.get_children():
-		if chip.clickable: usable_chips.push_back(chip)
-	
-	_trigger_move(usable_chips[0])
+func make_move(dice_roll:int, ai_chips, player_chips):
+	if ai_chips.size() > 0:
+		_trigger_move(ai_chips[0])
 
 func _trigger_move(chip):
 	chip.emit_signal("mouse_entered")
