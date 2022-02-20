fe3b25d306a218639c55203b65f041655a2c0629
Tch1b0 <Jpour3006@gmail.com>
2022-02-20 16:14:01 +0100

    add kjarrigan ai

--- components/AI/KjarriganAI.gd
+++ components/AI/KjarriganAI.gd
@@ -5,5 +5,16 @@ func _init():
 	self.name = "Kjarrigan"
 
 func make_move(dice_roll, ai_chips, player_chips, fields):
-	# implement AI move here
-	.make_move(dice_roll, ai_chips, player_chips, fields)
+	# first priority: move chips into the safety zone
+	for chip in ai_chips:
+		if chip.position + dice_roll >= 12:
+			_trigger_move(chip)
+			return
+
+	# second priority: move new chips in
+	for chip in ai_chips:
+		if chip.position == -1:
+			_trigger_move(chip)
+			return
+	
+	_trigger_move(ai_chips[0])
