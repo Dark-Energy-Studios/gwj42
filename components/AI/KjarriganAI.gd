8a4baa8b3ee955767bed60ec00b388032f30376f
Tch1b0 <Jpour3006@gmail.com>
2022-02-20 15:38:17 +0100

    add sceleton for kjarrigan ai implementation

--- components/AI/KjarriganAI.gd
+++ components/AI/KjarriganAI.gd
@@ -3,3 +3,7 @@ class_name KjarriganAI
 
 func _init():
 	self.name = "Kjarrigan"
+
+func make_move(dice_roll, ai_chips, player_chips, fields):
+	# implement AI move here
+	.make_move(dice_roll, ai_chips, player_chips, fields)
